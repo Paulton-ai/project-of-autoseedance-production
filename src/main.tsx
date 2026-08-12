8df76bdceb1a82e9a95ef4ebb5ca2828907ea9a6
Paulton-ai <paultonai26@gmail.com>
2026-08-12 11:06:40 +0500

    Hydrate the server-rendered document on the client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { hydrateRoot } from "react-dom/client";
+import { RouterClient } from "@tanstack/react-router/ssr/client";
 import { router } from "./router";
 import { initAnalytics, trackPageView } from "./lib/analytics";
-
-const queryClient = new QueryClient();
+import "./styles.css";
 
 initAnalytics();
 
@@ -13,18 +11,9 @@ router.subscribe("onResolved", ({ toLocation }) => {
   trackPageView(toLocation.pathname);
 });
 
-function App() {
-  useEffect(() => {
-    trackPageView(window.location.pathname);
-  }, []);
-  return <RouterProvider router={router} context={{ queryClient }} />;
-}
-
-const rootEl = document.getElementById("root")!;
-ReactDOM.createRoot(rootEl).render(
+hydrateRoot(
+  document,
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <RouterClient router={router} />
   </React.StrictMode>,
 );
